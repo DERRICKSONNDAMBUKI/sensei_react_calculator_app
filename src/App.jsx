48755d19ec23b7670257fc1a976e1d2049ac622b
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-05-12 08:23:14 +0300

    useState and functionality commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Button } from "./Button";
 import { ButtonBox } from "./ButtonBox";
 import { Screen } from "./Screen";
@@ -12,17 +13,33 @@ const btnValues = [
 ]
 
 function App() {
+
+  const [calc, setCalc] = useState({
+    sign:"",
+    num:0,
+    res:0,
+  })
   return (
    <Wrapper>
-     <Screen value= "0"/>
+     <Screen value= {calc.num ? calc.num : calc.res}/>
      <ButtonBox>
        {
          btnValues.flat().map((btn,i)=>{
            return (
                   <Button key={i} className={btn === "="?"equals":""} value = {btn} onClick={()=>{
-         console.log(`${btn} clicked!`);
-       }}
-       />
+                    btn === "C" 
+                    ? resetClickHandler 
+                    : btn === "+-" 
+                    ? invertClickHandler 
+                    : btn === "%" 
+                    ? percentClickHandler 
+                    : btn === "=" 
+                    ? equalsCLickHandler 
+                    : btn === "/" || btn === "X" || btn === "-" || btn === "+" 
+                    ? signClickHandler : btn === "." 
+                    ? commaClickHandler:numClickHandler
+                  }}
+                  />
            )
           
          })
